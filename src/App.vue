5631c10b85e7c375183e0dc105285ae2393e7d23
gabrielbvl <gabrielbvl2023@gmail.com>
2023-12-07 11:09:27 -0300

    Finished project

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,49 @@
 <template>
-  <div>
-    <h1>Hello Word</h1>
+  <div class="container grid-lg my-2 py-2 text-dark">
+    <div class="card">
+      <div class="cardHeader">
+        <div class="h4">Moedas</div>
+      </div>
+      <div class="cardBody">
+        <CoinData :quotes="quotes" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import CoinData from "./components/CoinData.vue";
+import api from "@/server/api.js";
+import { onMounted, reactive, toRefs } from "vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    CoinData,
+  },
+
+  setup() {
+    const data = reactive({
+      quotes: {},
+    });
+
+    onMounted(async () => {
+      const response = await api.all();
+      data.quotes = response.data;
+    });
+    return { ...toRefs(data) };
+  },
 };
 </script>
 
-<style></style>
+<style>
+.h4 {
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.card {
+  box-shadow: inset 0 0 1em gold, 0 0 2em silver;
+  padding: 10px;
+}
+</style>
---
+++ src/components/CoinData.vue
@@ -0,0 +1,50 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Code</th>
+        <th>Name</th>
+        <th>Max</th>
+        <th>Min</th>
+        <th>Variation</th>
+
+        <th></th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(quote, key) in quotes" :key="key">
+        <td>{{ key }}</td>
+        <td>{{ quote.name }}</td>
+        <td>{{ quote.high }}</td>
+        <td>{{ quote.low }}</td>
+
+        <td>
+          <span
+            class="label label-rounded text-small"
+            :class="{
+              'label-error': quote.pctChange < 0,
+              'label-success': quote.pctChange > 0,
+            }"
+          >
+            {{ quote.pctChange }} %
+          </span>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: { quotes: { type: Object, required: true } },
+};
+</script>
+
+<style scoped>
+span {
+  min-width: 70px;
+  text-align: center;
+  font-weight: 700;
+}
+</style>
